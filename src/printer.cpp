a7c98776ac417f3a87d0a45f3948b4122fefc6b3
Michal Kielan <michalkielan@protonmail.com>
2018-04-27 18:53:40 +0200

    make printer works

--- src/printer.cpp
+++ src/printer.cpp
@@ -3,17 +3,7 @@
 #include <QtPrintSupport/QPrinter>
 #include <QtPrintSupport/QPrintDialog>
 #include <QPainter>
-
-#include <QPdfWriter>
-
-#include <QDir>
-#include <QTemporaryDir>
-
-
-///
-#include <QFileDialog>
 #include <QTextDocument>
-///
 
 Printer::Printer(const PassData& passData) :
   passData(passData)
@@ -22,58 +12,16 @@ Printer::Printer(const PassData& passData) :
 
 void Printer::print()
 {
-  /// generate pdf
-//  QString fileName = QFileDialog::getSaveFileName(nullptr, "Export PDF", QString(), "*.pdf");
-//  if (QFileInfo(fileName).suffix().isEmpty()) { fileName.append(".pdf"); }
-
-//  QPrinter printer(QPrinter::PrinterResolution);
-//  printer.setOutputFormat(QPrinter::PdfFormat);
-//  printer.setPaperSize(QPrinter::A4);
- // printer.setOutputFileName(fileName);
-
-//  QTextDocument doc;
-
-//  doc.setHtml(passData.getPdfHtml());
-//  doc.setPageSize(printer.pageRect().size());
-//  doc.print(&printer);
-
-  /// create pdf
-  const QString tmpFileName = "tmp_pass_data.pdf";
-
-  QTemporaryDir tmp;
-  QDir separator;
-
-  QDir tmpFilePath = tmp.path() + separator.separator() + tmpFileName;
-  //QDir path; path.separator();
-
-  //QString fileName{QTemporaryDir::path() + QDir::separator() + "tmp_pass_data.pdf"};
-
-  QPdfWriter pdfwriter("");
-  pdfwriter.setPageSize(QPageSize(QPageSize::A4));
-  QPainter painter(&pdfwriter);
-
-
   QPrinter printer;
+  QTextDocument textDocument;
 
-//  QTextDocument doc;
+  textDocument.setHtml(passData.getPdfHtml());
 
-//  doc.setHtml(passData.getPdfHtml());
-//  doc.setPageSize(printer.pageRect().size());
-//  doc.print(&printer);
+  QPrintDialog printDialog(&printer);
+  printDialog.setWindowTitle(QDialog::tr("Print Passphrases"));
+  printDialog.setModal(true);
+  printDialog.exec();
 
-  QPrintDialog dialog(&printer);
-  dialog.setWindowTitle(QDialog::tr("Print Document"));
-  dialog.setModal(true);
-  dialog.exec();
-
-//  QPrinter printer(QPrinter::PrinterResolution);
-  printer.setOutputFormat(QPrinter::PdfFormat);
-  printer.setPaperSize(QPrinter::A4);
-  //printer.setOutputFileName(fileName);
-
-  QTextDocument doc;
-
-  doc.setHtml(passData.getPdfHtml());
-  doc.setPageSize(printer.pageRect().size());
-  doc.print(&printer);
+  textDocument.setPageSize(printer.pageRect().size());
+  textDocument.print(&printer);
 }
